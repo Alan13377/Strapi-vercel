386e6fe8366abb5c4d6422af6dfcce9c2888e7b2
Alan Gustavo <johnforge1337@gmail.com>
2022-10-23 18:11:58 -0500

    Plugins

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: "<vercel-api-token>",
-      appFilter: "your-app-name-on-vercel",
+        "https://api.vercel.com/v1/integrations/deploy/prj_b1HAV5Vu3aaRpNZsoTdrc3Q15cnV/2aPqbEQPpE",
+      apiToken: "NpKYvQpS3dDOfnZqkYOeZqd7",
+      appFilter: "strapi-vercel ",
       teamFilter: "your-team-id-on-vercel",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
