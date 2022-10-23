f0186feba16e84c83375a835285013faec396788
Alan Gustavo <johnforge1337@gmail.com>
2022-10-23 17:31:06 -0500

    modificacion ENV

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+        process.env.VER_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VER_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VER_DEPLOY_PLUGIN_APP_FILTER,
      
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
